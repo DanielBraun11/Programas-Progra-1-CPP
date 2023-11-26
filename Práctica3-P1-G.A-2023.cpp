0fa302f6b3607be0db37e2d24f6d9f098e987518
Daniel Braun <128066959+DanielBraun11@users.noreply.github.com>
2023-11-26 14:43:40 +0100

    Update Práctica3-P1-G.A-2023.cpp
    Menú para trabajar con letras y palabras a partir de unas frases introducidas por consola.

--- Práctica3-P1-G.A-2023.cpp
+++ Práctica3-P1-G.A-2023.cpp
@@ -1,21 +1,35 @@
+//PRACTICA 3 DEL GRUPO A
 #include <iostream>
 #include <array>
 #include <string>
 using namespace std;
 
+struct datosFrase{
+    string frase;
+    int longitud;
+};
+
+struct datosLetra{
+    char letra;
+    int contador;
+};
+
+struct datosPalabra{
+    string palabra;
+    int contador;
+    int numFrase;   //Numero de la frase donde aparece
+    int posFrase;
+};
 
 int main() {
     int opcion;
-    array<array<string, 2>, 10> matriz;  // 10 filas x 2 columnas
-    string frase;
+    int posicion;
+    bool primeraVez{true};
+    array<datosFrase,5> frases;
+    array<datosLetra,5> letras;
+    array<datosPalabra,5> palabras;
 
-
-    // Variables para la matriz de letras
-    array<array<string, 2>, 5> matrizLetras; // 5 filas x 2 columnas
-
-
-    // Menu de selección
-    do{
+    do {
         cout<<"Seleccione una opcion (1-4): "<<endl;
         cout<<"1. Matriz de frases"<<endl;
         cout<<"2. Informacion de letra"<<endl;
@@ -23,57 +37,131 @@ int main() {
         cout<<"4. Salir del programa"<<endl;
         cin>>opcion;
 
-        if(opcion < 1 || opcion > 4){
-            cout<<"La opcion no es valida. Por favor, vuelva a seleccionar otra opcion."<<endl;
-        }
-
         switch(opcion){
-        // Matriz de frases
         case 1:
-            cout<<"Introduce 10 frases:"<<endl;
+            cout<<"Introduzca 5 frases."<<endl;
             cin.ignore();
 
-            // Rellenar matriz
-            for(int fila{0}; fila < matriz.size(); fila++){
-                cout << "Frase " << fila + 1 << ": " << endl;
-                getline(cin, matriz.at(fila).at(0));
-                matriz.at(fila).at(1) = to_string(matriz.at(fila).at(0).size());
+            //Rellenar las frases
+            for(int i = 0; i < frases.size(); i++){
+                cout<<"Frase " << i + 1 << ": "<<endl;
+                getline(cin, frases.at(i).frase);
             }
 
-            // Mostrar matriz
-            cout<<"---------------------------"<<endl;
-            for(int i{0}; i < matriz.size(); i++){
-                cout << matriz.at(i).at(0)<<" ------TAMANO----->>"<<matriz.at(i).at(1)<<endl;
-            }
-            cout<<"---------------------------"<<endl;
             cout<<endl;
+            //Calcular el tamaño y mostrar la frase y este
+            for(int i = 0; i < frases.size(); i++){
+                cout<<"Frase "<< i + 1<< ": "<< frases.at(i).frase<<endl;
+                frases.at(i).longitud = frases.at(i).frase.size();
+                cout<<"Longitud de la frase "<< i + 1<< ": "<< frases.at(i).longitud<<endl;
+                cout<<endl;
+            }
+
+            cout<<"-------------------------------"<<endl;
+
             break;
 
-            // Informacion de letra
         case 2:
-            if(matriz.at(0).at(0).empty()){
-                cout << "La matriz de frases no se ha rellenado."<<endl;
+            //Comprobar que las frases se han rellenado
+            if(frases.at(0).frase.empty()){
+                cout<<"Las frases aun no se han rellenado."<<endl;
+                cout<<endl;
             }else{
-                cout<<"Introduce 5 letras (en minusculas):"<<endl;
-                cin.ignore();
+                //Relleno las letras
+                cout<<"Introduzca 5 letras."<<endl;
+                for(int x{0} ; x<letras.size() ; x++){
+                    cout<<"Letra "<<x+1<<": "<<endl;
+                    cin>>letras.at(x).letra;
+                    letras.at(x).contador = 0;  //Aprovecho e inicializo el contador de cada letra en 0
+                }
 
-            //Relleno las letras de la matriz
-            for(int z{0} ; z<5 ; z++){
-                getline(cin,matrizLetras.at(z).at(0));
+                //Busco y cuento las letras
+                for(int i{0} ; i<letras.size() ; i++){
+                    for(int j{0} ; j<frases.size() ; j++){
+                        posicion = frases.at(j).frase.find(letras.at(i).letra); //Buscar desde 0
+                        while(posicion != -1){
+                            letras.at(i).contador++;
+                            posicion = frases.at(j).frase.find(letras.at(i).letra, posicion+1);  //Si encuentra, buscar desde 1 posicion mas de la devuelta
+                        }
+
+                    }
+                }
+
+                //Muestro los datos optenidos de las letras
+                for(int i{0} ; i<letras.size() ; i++){
+                    cout<<"Letra "<<i+1<<": "<<letras.at(i).letra<<endl;
+                    cout<<"Veces que aparece la letra "<<i+1<<": "<<letras.at(i).contador<<endl;
+                    cout<<endl;
+                }
             }
 
+            cout<<"-------------------------------"<<endl;
 
-
-
-
-
-
-            }
             break;
 
+        case 3:
+            //Comprobar que las frases se han rellenado
+            if(frases.at(0).frase.empty()){
+                cout<<"Las frases aun no se han rellenado."<<endl;
+                cout<<endl;
+            }else{
+                //Rellenos las palabras
+                cout<<"Introduce 5 palabras."<<endl;
+                cin.ignore();
+                for(int x{0} ; x<palabras.size() ; x++){
+                    cout<<"Palabra "<<x+1<<": "<<endl;
+                    getline(cin,palabras.at(x).palabra);
+                    palabras.at(x).contador = 0;  //Aprovecho e inicializo el contador de cada palabra en 0
+                    palabras.at(x).posFrase = -1; //Aprovecho e inicializo la posicion de cada palabra en -1
+                    palabras.at(x).numFrase = -1; //Aprovecho e inicializo el numero de la frase de cada palabra en -1
+                }
 
+                cout<<endl;
+
+                for(int i{0} ; i<palabras.size() ; i++){
+                    primeraVez = true;
+                    for(int j{0} ; j<frases.size() ; j++){
+                        posicion = frases.at(j).frase.find(palabras.at(i).palabra);
+                        while(posicion != -1){
+                            palabras.at(i).contador++;
+                            //posicion = frases.at(j).frase.find(palabras.at(i).palabra, posicion+1);
+                            if(primeraVez == true){
+                                palabras.at(i).numFrase = j;
+                                palabras.at(i).posFrase = posicion;
+                                primeraVez = false;
+                            }
+                            posicion = frases.at(j).frase.find(palabras.at(i).palabra, posicion+1);
+                        }
+                    }
+                }
+
+                //Muestro los datos optenidos de las palabras
+                for(int i{0} ; i<palabras.size() ; i++){
+                    cout<<"Palabra "<<i+1<<": "<<palabras.at(i).palabra<<endl;
+                    if(palabras.at(i).contador > 0){
+                        cout<<"Veces que aparece la palabra "<<i+1<<": "<<palabras.at(i).contador<<endl;
+                        cout<<"Posicion de la palabra "<<i+1<<": "<<palabras.at(i).posFrase<<endl;
+                        cout<<"Numero de frase donde aparece la palabra "<<i+1<<": "<<palabras.at(i).numFrase<<endl;
+                    }else{
+                        cout<<"Palabra no encontrada en las frases."<<endl;
+                    }
+                    cout<<endl;
+                }
+            }
+
+            cout<<"-------------------------------"<<endl;
+
+            break;
+
+        case 4:
+            //Salir del programa
+            break;
+
+        default:
+            cout << "La opcion no es valida. Por favor, vuelva a seleccionar otra opcion." << endl;
         }
-    }while(opcion != 4);
+
+    } while (opcion != 4);
 
     return 0;
 }
